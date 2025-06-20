c4c0be8534e2794326ad00806b8cbe9c9b32b80b
latihan-1 <beta22715@gmail.com>
2025-06-20 14:11:02 +0700

    tahap 6 dan 7,5

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import {neon} from '@neondatabase/serverless';
 
 export default function Page() {
+  async function create(formData: FormData) {
+    'use server';
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+    // Insert the comment from the form into the Postgres database
+    await sql`INSERT INTO comments (comment) VALUES (${comment})`;
+  }
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -46,6 +56,11 @@ export default function Page() {
             className="block md:hidden"
             alt="Screenshot of the dashboard project showing mobile version"
           />
+
+          <form action={create}>
+            <input type="text" placeholder="write a comment" name="comment" />
+            <button type="submit">Submit</button>
+          </form>
         </div>
       </div>
     </main>
